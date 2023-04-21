29d4fcd05803011848ee10b1e49f7b61ab9587a6
Enoch <enochdavid6699@gmail.com>
2023-04-22 00:37:54 +0530

    create router and controller for users

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+
 const homeController = require('../controller/home_controller');
+
 router.get('/' , homeController.home);
+
+//For any further routes access from here
+//router.use('/router_name' , require('./router_file') )
+
+router.use('/users' , require('./users'));
+
 module.exports=router;
