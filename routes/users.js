50bbee29e8595a31e291271c503825e5d3ebb275
Enoch <enochdavid6699@gmail.com>
2023-05-11 00:07:26 +0530

    Basic Styling

--- routes/users.js
+++ routes/users.js
@@ -17,6 +17,7 @@ router.post('/create-session', passport.authenticate(
     {failureRedirect: '/users/sign-in'},
 ) , usersController.createSession);
 
+
 router.get('/sign-out' , usersController.signOut);
 
 module.exports=router; 
